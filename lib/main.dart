b4f7fc74a8a191ca817ae4ad6dee952eb3added3
Emrys02 <Emrys_02@outlook.com>
2022-02-11 15:10:12 +0100

    Navigation bar added to home screen but pending changes remain for the favourites list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
-import 'package:cookbook/screens/meal_recipe.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/available_meals.dart';
 import 'widget/categories_list.dart';
+import 'screens/meal_recipe.dart';
+import 'widget/favourites_list.dart';
 
 void main() => runApp(_CookBookApp());
 
-class _CookBookApp extends StatelessWidget {
+class _CookBookApp extends StatefulWidget {
+  @override
+  State<_CookBookApp> createState() => _CookBookAppState();
+}
+
+class _CookBookAppState extends State<_CookBookApp> {
+  final List<Widget> _pages = [CategoriesList(), FavouritesList()];
+
+  int selectedPageIndex = 0;
+
+  void _selectpage(int value) {
+    setState(() {
+      selectedPageIndex = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.menu_rounded),
-            title: const Text('CookBook'),
-          ),
-          body: CategoriesList()),
+        appBar: AppBar(
+          leading: const Icon(Icons.menu_rounded),
+          title: const Text('CookBook'),
+        ),
+        body: _pages[selectedPageIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category_rounded),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_rounded), 
+                label: 'Favourites',
+            )
+          ],
+          onTap: (index) => _selectpage(index),
+          currentIndex: selectedPageIndex,
+        ),
+      ),
       routes: {
         AvailableMeals.route(): (context) => AvailableMeals(),
-        MealRecipe.route():(context) => MealRecipe(),
+        MealRecipe.route(): (context) => MealRecipe(),
       },
-    ); // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
---
+++ lib/widget/favourites_list.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class FavouritesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("No Favourites Selected");
+  }
+}
